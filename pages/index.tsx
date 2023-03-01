ee296740070b047bf1d7b703ec4c3b8d235d157e
MiwMiw1 <77629887+MiwMiw1@users.noreply.github.com>
2023-03-02 00:48:02 +0100

    nextjs & tailwind setup

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-lime-400">
+        <div className="h-[200px]">
+          eee
+        </div>
+    </div>
+  )
+}
