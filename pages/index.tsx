52b6575c52d4f6e4570d95cd65389ed4ec604d37
MiwMiw1 <77629887+MiwMiw1@users.noreply.github.com>
2023-03-04 15:59:36 +0100

    Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center space-x-6">
-      <a
+      <Link
         href="/firstmethod"
         className="w-[200px] h-[50px] bg-emerald-900 flex justify-center items-center rounded-lg text-white text-xl font-sm"
       >
         First Method
-      </a>
-      <a
+      </Link>
+      <Link
         href="/secondmethod"
         className="w-[200px] h-[50px] bg-emerald-900 flex justify-center items-center rounded-lg text-white text-xl font-sm"
       >
         Second Method
-      </a>
+      </Link>
     </div>
   );
 }
